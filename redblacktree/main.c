ed09161abed836bb057e93f30044ebdb6c600235
Franciszek Pietrusiak <fp458987@students.mimuw.edu.pl>
2024-05-30 13:08:01 +0200

    add comments

--- redblacktree/main.c
+++ redblacktree/main.c
@@ -1,5 +1,14 @@
 /*
-    Program do zabawy z bibliotkeką
+    Autor: Franciszek Pietrusiak
+    Program do zabawy z biblioteką
+    W nieskończonej pętli udostępnia dwie operacja:
+        [1] - dodaj node'a o danym kluczu do drzewa
+        [2] - usun node'a o danym kluczu z drzewa
+        [3] - sprawdź, czy istnieje w drzewie node z danym kluczem
+    Po każdej operacji wyświetlana jest struktura drzewa w formacie [klucz, kolor]
+
+    Kompilacja:
+    gcc -o main rb_tree.c main.c
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,10 +24,16 @@ int main () {
         if (op == 1) {
             insert(&root, klu);
         }
-        else {
+        else if (op == 2) {
             pnode v = find(root, klu);
             if (v != NIL)
                 deletenode(&root, v);
+        }
+        else if (op == 3) {
+            pnode v = find(root, klu);
+            if (v == NIL)
+                printf("NIE\n");
+            else printf("TAK\n");
         }
         info(root);
     }
--- redblacktree/rb_tree.c
+++ redblacktree/rb_tree.c
@@ -1,4 +1,8 @@
 /*
+    Autor: Franciszek Pietrusiak
+    Korzystałem z https://dl.ebooksworld.ir/books/Introduction.to.Algorithms.4th.Leiserson.Stein.Rivest.Cormen.MIT.Press.9780262046305.EBooksWorld.ir.pdf
+    w szczególności opisane są tam Przypadki 1-3 i Przypadki 1-4 odpowiednio w napraw_insert() i napraw_deletenode()
+    
     Drzewo Czerwono-Czarne musi spełniać warunki:
     -[1] Każdy node jest czerwony lub czarny
     -[2] Korzeń jest czarny
@@ -14,14 +18,14 @@ pnode NIL = NULL;
 
 pnode init() {
     if (NIL == NULL) {
-        NIL = (pnode)malloc(sizeof(node));
+        NIL = (pnode)malloc(sizeof(node)); // NIL jest strażnikiem, który przypomina NULL'a, ale jego kolor jest zawsze czarny
         NIL->kolor = BLACK;
         NIL->klucz = 0;
         NIL->lewy = NULL;
         NIL->prawy = NULL;
         NIL->p = NULL;
     }
-    return NIL;
+    return NIL; // korzeniem każdego pustego drzewa jest NIL
 }
 
 pnode newnode(int klu, int kol) {
@@ -48,34 +52,34 @@ void info(pnode root) {
     printf("\n");
 }
 
-void rotacja_w_lewo(pnode *root, pnode x) {
-    pnode y = x->prawy;
-    x->prawy = y->lewy; // przeszczep lewego syna y-ka jako prawego syna x-a
+void rotacja_w_lewo(pnode *root, pnode v) {
+    pnode y = v->prawy;
+    v->prawy = y->lewy; // przeszczep lewego syna y-ka jako prawego syna v
     if (y->lewy != NIL) // i jeśli on istnieje ...
-        y->lewy->p = x; // ... to mu to powiedz
-    y->p = x->p; // ojcem y-ka jest ojciec x-a
-    if (x->p == NIL) // jeśli x był korzeniem ...
+        y->lewy->p = v; // ... to mu to powiedz
+    y->p = v->p; // ojcem y-ka jest ojciec v
+    if (v->p == NIL) // jeśli v był korzeniem ...
         *root = y; // ... y jest korzeniem
-    else if (x == x->p->lewy) // jeśli x był lewym synem swojego ojca ...
-        x->p->lewy = y; // ... to teraz y nim jest
-    else x->p->prawy = y; // jeśli x był prawym synem swojego ojca, to analogicznie
-    y->lewy = x; // x staje się lewym synem y-ka
-    x->p = y; // ... i mu o tym mówimy
+    else if (v == v->p->lewy) // jeśli v był lewym synem swojego ojca ...
+        v->p->lewy = y; // ... to teraz y nim jest
+    else v->p->prawy = y; // jeśli v był prawym synem swojego ojca, to analogicznie
+    y->lewy = v; // v staje się lewym synem y-ka
+    v->p = y; // ... i mu o tym mówimy
 }
 
-void rotacja_w_prawo(pnode *root, pnode x) {
-    pnode y = x->lewy;
-    x->lewy = y->prawy; // przeszczep prawego syna y-ka jako lewego syna x-a
+void rotacja_w_prawo(pnode *root, pnode v) {
+    pnode y = v->lewy;
+    v->lewy = y->prawy; // przeszczep prawego syna y-ka jako lewego syna v
     if (y->prawy != NIL) // i jeśli on istnieje ...
-        y->prawy->p = x; // ... to mu to powiedz
-    y->p = x->p;
-    if (x->p == NIL)
+        y->prawy->p = v; // ... to mu to powiedz
+    y->p = v->p;
+    if (v->p == NIL)
         *root = y;
-    else if (x == x->p->lewy)
-        x->p->lewy = y;
-    else x->p->prawy = y;
-    y->prawy = x;
-    x->p = y;
+    else if (v == v->p->lewy)
+        v->p->lewy = y;
+    else v->p->prawy = y;
+    y->prawy = v;
+    v->p = y;
 }
 
 void napraw_insert(pnode *root, pnode v) {
@@ -123,131 +127,134 @@ void napraw_insert(pnode *root, pnode v) {
 void insert(pnode *root, int klu) {
     pnode v = *root;
     pnode y = NIL;
-    while (v != NIL) {
+    while (v != NIL) {  // schodzimy v aż do NIL'a
         y = v;
         if (klu < v->klucz)
             v = v->lewy;
         else v = v->prawy;
     }
     pnode u = newnode(klu, RED);
-    u->p = y;
-    if (y == NIL)
+    u->p = y; // ojcem u jest y
+    if (y == NIL) // jeśli drzewo jest puste
         *root = u;
-    else if (u->klucz < y->klucz)
+    else if (u->klucz < y->klucz) // inaczej ustaw u jako właściwego syna y
         y->lewy = u;
     else y->prawy = u;
-    napraw_insert(root, u);
+    napraw_insert(root, u); // napraw drzewo
 }
 
 void przeszczep(pnode *root, pnode v, pnode u) {
-    if (v->p == NIL)
+    if (v->p == NIL) // jeśli v jest korzeniem
         *root = u;
-    else if (v == v->p->lewy)
+    else if (v == v->p->lewy) // inaczej podmień v przez u dla ojca v
         v->p->lewy = u;
     else v->p->prawy = u;
-    u->p = v->p;
+    u->p = v->p; // i powiedz u, że zmienił mu się ojciec
 }
 
 pnode minimum(pnode v) {
-    while (v->lewy != NIL)
+    while (v->lewy != NIL) // idź do lewego syna dopóki się da
         v = v->lewy;
     return v;
 }
 
-void napraw_deletenode(pnode *root, pnode x) {
-    while (x != *root && x->kolor == BLACK) {
-        if (x == x->p->lewy) {
-            pnode w = x->p->prawy;
-            if (w->kolor == RED) {
+void napraw_deletenode(pnode *root, pnode v) {
+    while (v != *root && v->kolor == BLACK) {
+        if (v == v->p->lewy) { // jeśli v jest lewym synem
+            pnode w = v->p->prawy; // w jest prawym bratem v
+            if (w->kolor == RED) { // Przypadek 1
                 w->kolor = BLACK;
-                x->p->kolor = RED;
-                rotacja_w_lewo(root, x->p);
-                w = x->p->prawy;
+                v->p->kolor = RED;
+                rotacja_w_lewo(root, v->p);
+                w = v->p->prawy;
             }
-            if (w->lewy->kolor == BLACK && w->prawy->kolor == BLACK) {
+            if (w->lewy->kolor == BLACK && w->prawy->kolor == BLACK) { // Przypadek 2
                 w->kolor = RED;
-                x = x->p;
+                v = v->p;
             }
             else {
-                if (w->prawy->kolor == BLACK) {
+                if (w->prawy->kolor == BLACK) { // Przypadek 3
                     w->lewy->kolor = BLACK;
                     w->kolor = RED;
                     rotacja_w_prawo(root, w);
-                    w = x->p->prawy;
-                }
-                w->kolor = x->p->kolor;
+                    w = v->p->prawy;
+                } // Przypadek 4
+                w->kolor = v->p->kolor;
                 w->p->kolor = BLACK;
                 w->prawy->kolor = BLACK;
                 rotacja_w_lewo(root, w);
-                x = *root;
+                v = *root;
             }
         }
-        else {
-            pnode w = x->p->lewy;
+        else { // symetryczna sytuacja
+            pnode w = v->p->lewy;
             if (w->kolor == RED) {
                 w->kolor = BLACK;
-                x->p->kolor = RED;
-                rotacja_w_prawo(root, x->p);
-                w = x->p->lewy;
+                v->p->kolor = RED;
+                rotacja_w_prawo(root, v->p);
+                w = v->p->lewy;
             }
             if (w->prawy->kolor == BLACK && w->prawy->kolor == BLACK) {
                 w->kolor = RED;
-                x = x->p;
+                v = v->p;
             }
             else {
                 if (w->lewy->kolor == BLACK) {
                     w->prawy->kolor = BLACK;
                     w->kolor = RED;
                     rotacja_w_lewo(root, w);
-                    w = x->p->lewy;
+                    w = v->p->lewy;
                 }
-                w->kolor = x->p->kolor;
+                w->kolor = v->p->kolor;
                 w->p->kolor = BLACK;
                 w->lewy->kolor = BLACK;
                 rotacja_w_prawo(root, w);
-                x = *root;
+                v = *root;
             }
         }
     }
-    x->kolor = BLACK;
+    v->kolor = BLACK;
 }
 
-void deletenode(pnode *root, pnode z) {
+void deletenode(pnode *root, pnode v) {
     pnode x;
-    pnode y = z;
-    int y_original_kolor = y->kolor;
-    if (z->lewy == NIL) {
-        x = z->prawy;
-        przeszczep(root, z, z->prawy);
+    pnode y = v;
+    int y_original_kolor = y->kolor; // y może zmienić kolor, dlatego trzeba zapiamiętać jego początkowy kolor
+    if (v->lewy == NIL) { // jak v nie ma lewego syna
+        x = v->prawy;
+        przeszczep(root, v, v->prawy); // to zastąp v przez jego prawego syna (może być NIL)
     }
-    else if (z->prawy == NIL) {
-        x = z->lewy;
-        przeszczep(root, z, z->lewy);
+    else if (v->prawy == NIL) { // symetrycznie gdy v nie ma prawego syna
+        x = v->lewy;
+        przeszczep(root, v, v->lewy);
     }
-    else {
-        y = minimum(z->prawy);
+    else { // jeśli v ma obu synów
+        y = minimum(v->prawy); // y zastąpi v
         y_original_kolor = y->kolor;
         x = y->prawy;
-        if (y != z->prawy) {
-            przeszczep(root, y, y->prawy);
-            y->prawy = z->prawy;
-            y->prawy->p = y;
+        if (y != v->prawy) { // jeśli y nie jest korzeniem poddrzewa v->prawy
+            przeszczep(root, y, y->prawy); // >
+            y->prawy = v->prawy;           // | zastępuję y przez jego prawego syna (wiadomo, że y nie ma lewego syna)
+            y->prawy->p = y;               // >
         }
-        else x->p = y;
-        przeszczep(root, z, y);
-        y->lewy = z->lewy;
-        y->lewy->p = y;
-        y->kolor = z->kolor;
+        else x->p = y; // jeśli jest korzeniem poddrzewa v->prawy
+        przeszczep(root, v, y); // >
+        y->lewy = v->lewy;      // | zastępuję v przez y
+        y->lewy->p = y;         // |
+        y->kolor = v->kolor;    // >
     }
+    // tylko jeśli albo v był czarny i go usunęliśmy (gdy v nie ma jednego syna)
+    // lub y był czarny, ale zastąpił v (gdy v ma obu synów)
+    // to może dojść do naruszenia [5] warunku
     if (y_original_kolor == BLACK)
         napraw_deletenode(root, x);
-    free(z);
+    free(v); // zwolnij pamięć
 }
 
 pnode find(pnode v, int klu) {
     if (v == NIL) return NIL;
-    if (v->klucz == klu) return v;
-    if (klu < v->klucz)
+    if (v->klucz == klu) return v; // znalazłem node'a
+    if (klu < v->klucz) // pojdź do jednego z synów
         return find(v->lewy, klu);
     else return find(v->prawy, klu);
 }
