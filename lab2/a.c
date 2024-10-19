317699219de31b58320a80ff95a7b3adb86d37c7
Franciszek Pietrusiak <fp458987@students.mimuw.edu.pl>
2024-10-19 20:18:46 +0200

    lab2 done

--- lab2/a.c
+++ lab2/a.c
@@ -12,12 +12,46 @@ int nwd(int a, int b) {
 
 int pozycja(int a, int b) {
     if (a < b) swap(&a, &b);
-
+    if (a == 1 && b == 1)
+        return 0;
     
+    int na, nb, d;
+    int op1, op2;
 
+    // opcja 1
+    d = nwd(a-1, b);
+    na = (a-1) / d;
+    nb = b / d;
+    op1 = pozycja(na, nb);
+
+    if (op1 == 0)
+        return 1;
+    
+    // opcja 2
+    if (b > 1) {
+        d = nwd(a, b-1);
+        na = a / d;
+        nb = (b-1) / d;
+        op2 = pozycja(na, nb);
+    }
+    else {
+        return 0;
+    }
+
+    if (op2 == 0)
+        return 1;
+    else return 0;
 }
 
 int main() {
+
+    int L = 100;
+    int R = 110;
+    for (int i=L; i<=R; i++) {
+        for (int j=L; j<=R; j++)
+            printf("%d", pozycja(i, j));       
+        printf("\n");
+    }
 
     return 0;
 }
--- lab2/gra.c
+++ lab2/gra.c
@@ -1,60 +1,20 @@
-#include <stdio.h>
-#define LIMIT 1003
-
-int wynik[LIMIT][LIMIT];
-
-void swap(int *a, int *b) {
-    *b ^= *a;
-    *a ^= *b;
-    *b ^= *a;
-}
+// Autor: Franciszek Pietrusiak
 
 int nwd(int a, int b) {
-    return (b == 0 ? a : nwd(b, a % b));
+    return (b == 0 ? a : nwd(b, a%b));
 }
 
-void zapamietaj(int a, int b, int c) {
-    if (a < LIMIT && wynik[a][b] == 0)
-        wynik[a][b] = c;
+// Zwraca 1 jesli pozycja (a, b) jest wygrywajaca, w przeciwnym przypadku zwraca 0
+int pozycja(int a, int b) {
+    int d = nwd(a, b);
+    a /= d; b /= d;
+    return ((a+b) % 2 == 1);
 }
 
-int jakapozycja(int a, int b) {
-    if (a < b) swap(&a, &b);
-    if (a < LIMIT && wynik[a][b] != 0) {
-        return wynik[a][b] - 1;
-    }
-    printf("%d %d\n", a, b);
-
-    if (a == 1 && b == 1) {
-        zapamietaj(1, 1, 1);
-        return 0;
-    }
-    if (b == 1) {
-        int res = (a % 2 == 0 ? 1 : 0);
-        zapamietaj(a, 1, res + 1);
-        return res;
-    }
-    int d = nwd(a-1, b);
-    int opcja1 = jakapozycja((a-1)/d, b/d);
-    zapamietaj((a-1)/d, b/d, opcja1+1);
-    int opcja2 = 0;
-    if (b > 1) {
-        d = nwd(a, b-1);
-        opcja2 = jakapozycja(a/d, (b-1)/d);
-        zapamietaj(a/d, (b-1)/d, opcja2+1);
-    }
-    return (opcja1 + opcja2 == 0);
-}
-
-/*
 int graj(int a, int b) {
-
-
-} */
-
-int main () {
-
-    printf("%d\n", jakapozycja(1237,123));
-
-    return 0;
+    if (a == 1 && b == 1)
+        return 0;
+    if (pozycja(a-1, b) == 0) // jesli (a-1, b) jest przegrywajaca to nalezy wyslac tam drugiego gracza
+        return 1;
+    else return 2;
 }
