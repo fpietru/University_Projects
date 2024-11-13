5243d7bdd5c00696cf9d7054caa8ca6d2120b06d
Franciszek Pietrusiak <fp458987@neptun.mimuw.edu.pl>
2024-11-13 15:45:58 +0100

    done with task from lab4

--- lab4/run.sh
+++ lab4/run.sh
@@ -1,12 +1,12 @@
 g++ -o gen gen.cpp
 gcc -o brut brut.c
-gcc -o a a.c
+gcc -o trz trz.c
 i=0
 while true
 do
     ./gen $i > in
     ./brut < in > out1
-    ./a < in > out2
+    ./trz < in > out2
 	
     if diff out1 out2 > res; then
 		echo "OK $i"
