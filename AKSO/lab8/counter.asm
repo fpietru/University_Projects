4b6a6cf3c6418f8a81159c2b990e72c454cf0c39
Franciszek Pietrusiak <fp458987@students.mimuw.edu.pl>
2025-05-11 12:38:35 +0200

    works on students

--- AKSO/lab8/counter.asm
+++ AKSO/lab8/counter.asm
@@ -1,15 +1,15 @@
-section .data
-    count:  dq 0       ; 64-bitowy licznik wywołań
+section .bss
+    count:  resq 1       ; 64-bitowy licznik wywołań
 
 section .text
     global counter
 
 counter:
     ; Zwiększ wartość pod count o 1
-    inc qword [count]
+    inc qword [rel count]
 
     ; Załaduj aktualną wartość (po inkrementacji) do rejestru RAX
-    mov rax, [count]
+    mov rax, [rel count]
 
     ; Zwróć wynik jako unsigned int (tylko dolne 32 bity RAX są używane)
     ret
