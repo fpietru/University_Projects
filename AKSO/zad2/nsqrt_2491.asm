111b74ec73740c4695beb3f4b1b5a190e0480bbf
Franciszek Pietrusiak <fp458987@students.mimuw.edu.pl>
2025-05-13 00:08:59 +0200

    added macro, time, space opt

--- AKSO/zad2/nsqrt_2491.asm
+++ AKSO/zad2/nsqrt_2491.asm
@@ -21,15 +21,6 @@ global nsqrt
 ; rax - y := zmienna lokalna
 ; rcx - z := zmienna lokalna
 
-%macro clean 2                       ; for (i=0; i<%2; i++) { (%1)[i] = 0 }
-	push rdi
-	mov rdi, %1
-	mov rcx, %2
-	xor rax, rax
-	rep stosq 
-	pop rdi
-%endmacro
-
 nsqrt:
 	push rbx
 	push r12
@@ -37,58 +28,80 @@ nsqrt:
 	push r14
 	push r15
 
-	mov r8, rdx                  ; r8 - s := dlugosc tablicy Q
-	shr r8, 6                    ; s = n / 64 = n >> 6
-	lea r9, [r8 + r8]            ; r9 - ss = 2 * s := dlugosc tablicy X, T
-	lea rbx, [rel T]             ; rbx := adres tablicy T
+	mov r8, rdx ; r8 - s := dlugosc tablicy Q
+	shr r8, 6 ; s = n / 64 = n >> 6
+	lea r9, [r8 + r8] ; r9 - ss = 2 * s := dlugosc tablicy X, T
+	lea rbx, [rel T] ; rbx := adres tablicy T
 
-	clean rdi, r8                ; czyszczenie Q
+	; czyszczenie Q
+	mov r10, 0                 ; r10 - i = 0
+.clean_Q_loop:                     ; for (i=0; i<s; i++)
+	mov qword [rdi + r10*8], 0 ; Q[i] = 0
+	inc r10
+	cmp r10, r8
+	jb .clean_Q_loop
 
-	                             ; iteracyjnie wyliczanie Q
-	mov r11, 1                   ; r11 - j = 1
-.main_loop:                          ; for (j=1; j<=n; j++)
+	; iteracyjnie wyliczanie Q
+	mov r11, 1                 ; r11 - j = 1
+.main_loop:                        ; for (j=1; j<=n; j++)
 		
-	                             ; r12 - k := n-j+1
-	mov r12, rdx                 ; k = n
-	sub r12, r11                 ; k -= j
-	inc r12                      ; k++
+	; r12 - k := n-j+1
+	mov r12, rdx ; k = n
+	sub r12, r11 ; k -= j
+	inc r12      ; k++
 
-	                             ; r13 - l := k / 64 = k >> 6
-	mov r13, r12                 ; l = k
-	shr r13, 6                   ; l >>= 6
+	; r13 - l := k / 64 = k >> 6
+	mov r13, r12 ; l = k
+	shr r13, 6   ; l >>= 6
 
-	clean rbx, r9                ; czyszczenie T
+	; czyszczenie T
+	mov r10, 0                 ; r10 - i = 0
+.clean_T_loop_1:                   ; for (i=0; i<l; i++)
+	mov qword [rbx + r10*8], 0 ; T[i] = 0
+	inc r10
+	cmp r10, r13
+	jb .clean_T_loop_1
+	
+	mov r10, r8
+	add r10, r13
+.clean_T_loop_2:                   ; for (i=s+l; i<ss; i++)
+	mov qword [rbx + r10*8], 0 ; T[i] = 0
+	inc r10
+	cmp r10, r9
+	jb .clean_T_loop_2
 
-.r_shift:                            ; T += 2^(n-j+1)*Q
+.r_shift: ; T += 2^(n-j+1)*Q
 
-	                             ; r14 - r := k % 6 = k & 63
-	mov r14, r12                 ; r = k
-	and r14, 63                  ; r &= 63
+	; r14 - r := k % 6 = k & 63
+	mov r14, r12 ; r = k
+	and r14, 63  ; r &= 63
 
+	; i = s-1
 	mov r10, r8
 	dec r10
-.r_shift_loop:                       ; for (i=s-1; i>=0; i--)
-	mov r15, qword [rdi + r10*8] ; x = Q[i]
+.r_shift_loop:
+	; r15 - x := Q[i]
+	mov r15, qword [rdi + r10*8]
 
-	                             ; T[i+l] = y = x << r
+	; T[i+l] = y = x << r
 	mov rax, r15                 ; y = x
 	mov cl, r14b                 ; z = r
 	shl rax, cl                  ; x <<= z
 	lea rcx, [r10 + r13]         ; z = i + l
 	mov qword [rbx + rcx*8], rax ; T[z] = x
 
-	                             ; if (r != 0 && i+l+1 < ss)
+	; if (r != 0 && i+l+1 < ss)
 	test r14, r14
 	jz .skip                     ; if (r == 0)
 
-	                             ; y = i+l+1
+	; rax - y = i+l+1
 	mov rax, r10                 ; y = i
 	add rax, r13                 ; y += l
 	inc rax                      ; y++
 	cmp rax, r9
 	jae .skip                    ; if (y >= ss)
 
-	                             ; T[i+l+1] |= Q[i] >> (64 - r)
+	; T[i+l+1] |= Q[i] >> (64 - r)
 	mov rax, r15                 ; y = x = Q[i]
 	mov cl, 64                   ; z = 64
 	sub cl, r14b                 ; z -= r
@@ -118,7 +131,7 @@ nsqrt:
 
 	mov r10, r12                 ; i = k
 	shr r10, 6                   ; i >>= 6
-.add_bit_T_loop:                     ; for (i=k/64; i<ss; i++)
+.add_bit_T_loop: ; for (i=k/64; i<ss; i++)
 	mov r13, [rbx + r10*8]       ; l = T[i]
 	add [rbx + r10*8], r14       ; T[i] += r
 	cmp [rbx + r10*8], r13
@@ -133,7 +146,7 @@ nsqrt:
 	mov r10, r9                  ; i = ss
 	dec r10                      ; i--
 .compare_loop:
-	                             ; X[i] ? T[i]
+	; X[i] ? T[i]
 	mov r12, [rsi + r10*8]       ; k = X[i]
 	mov r13, [rbx + r10*8]       ; l = T[i]
 	cmp r12, r13
@@ -144,22 +157,23 @@ nsqrt:
 .break_compare_loop:
 
 .add_bit_Q:
-	                             ; k = n-j
+	; k = n-j
 	mov r12, rdx                 ; k = n
 	sub r12, r11                 ; k -= r11
 
-	                             ; l = k % 64
+	; l = k % 64
 	mov r13, r12                 ; l = k
 	and r13, 63                  ; l &= 63
 
-	                             ; r = 2^l
+	; r = 2^l
 	mov r14, 1
 	mov cl, r13b
 	shl r14, cl
 
+	; i = k / 64
 	mov r10, r12
 	shr r10, 6
-.add_bit_Q_loop:                     ; for (i=i/64; i<s; i++)
+.add_bit_Q_loop:
 	mov r13, [rdi + r10*8]       ; l = Q[i]
 	add [rdi + r10*8], r14       ; Q[i] += r
 	cmp [rdi + r10*8], r13
