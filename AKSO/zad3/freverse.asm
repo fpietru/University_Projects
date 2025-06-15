d0df2cba34951cbad8f1e20335b87baff1dfaa76
Franciszek <f.pietrusiak@student.uw.edu.pl>
2025-06-15 16:35:42 +0200

    new version

--- AKSO/zad3/freverse.asm
+++ AKSO/zad3/freverse.asm
@@ -1,140 +1,151 @@
-section .text
+; Autor: Franciszek Pietrusiak
 global _start
+section .text
 
-; Stałe dla wywołań systemowych i flag
-O_RDWR      equ 2
-PROT_READ   equ 1
-PROT_WRITE  equ 2
-MAP_SHARED  equ 1
-MS_SYNC     equ 4
-S_IFREG     equ 0x8000     ; Stała dla zwykłego pliku
+; Stale dla wywolan systemowych i flag
+O_RDWR      equ 2       ; sys_open: odczyt i zapis
+PROT_READ   equ 1       ; sys_mmap: odczyt pliku 
+PROT_WRITE  equ 2       ; sys_mmap: zapis do pliku
+MAP_SHARED  equ 1       ; sys_mmap: dzielone mapowanie pliku
+MS_SYNC     equ 4       ; sys_msync: wymusza natychmiastowy zapis
+STAT_SIZE   equ 144     ; sys_fstat: rozmiar struct stat
+ST_MODE     equ 24      ; sys_fstat: typ pliku i prawa
+ST_SIZE     equ 48      ; sys_fstat: rozmiar pliku
+S_IFREG     equ 0x8000  ; kod zwyklego pliku
+
+; Numery funkcji systemowych
+SYS_OPEN    equ 2
+SYS_CLOSE   equ 3
+SYS_FSTAT   equ 5
+SYS_MMAP    equ 9
+SYS_MUNMAP  equ 11
+SYS_MSYNC   equ 26
+SYS_EXIT    equ 60
 
 _start:
-    ; Sprawdzenie liczby argumentów
+    ; Sprawdza liczbe argumentow
     pop rcx                 ; rcx = argc
-    cmp rcx, 2              ; oczekujemy dokładnie 2 argumenty
-    jne .error              ; jeśli nie, błąd
+    cmp rcx, 2              ; program ma dostac dokladnie 2 argumenty
+    jne .error              ; jesli tak nie jest => blad
 
-    ; Pobranie nazwy pliku
-    pop rdi                 ; argv[0] (nazwa programu)
-    pop rdi                 ; argv[1] (nazwa pliku)
+    ; Pobiera nazwy
+    pop rdi                 ; argv[0] = nazwa programu
+    pop rdi                 ; argv[1] = nazwa pliku
 
-    ; Otwarcie pliku
-    mov rax, 2              ; sys_open
-    mov rsi, O_RDWR         ; flagi: O_RDWR
-    xor rdx, rdx            ; tryb dostępu: 0
+    ; Otwiera plik
+    mov rax, SYS_OPEN       ; sys_open
+    mov rsi, O_RDWR         ; flaga: O_RDWR
+    xor rdx, rdx            ; tryb dostepu = 0
     syscall
-    test rax, rax           ; sprawdź błąd
-    js .error               
-    mov r12, rax            ; zapisz deskryptor
+    test rax, rax           ; sprawdza blad
+    js .error
+    mov r12, rax            ; zapisuje deskryptor
 
-    ; Pobranie informacji o pliku
-    sub rsp, 144            ; rezerwacja miejsca na struct stat
-    mov rax, 5              ; sys_fstat
+    ; Pobiera informacje o pliku
+    sub rsp, STAT_SIZE      ; rezerwuje bufor na struct stat
+    mov rax, SYS_FSTAT      ; sys_fstat
     mov rdi, r12            ; deskryptor pliku
-    mov rsi, rsp            ; wskaźnik na struct stat
+    mov rsi, rsp            ; wskaznik na struct stat
     syscall
-    test rax, rax           ; sprawdź błąd
-    js .fstat_error         
+    test rax, rax
+    js .revert_stack
 
-    ; Sprawdź czy to zwykły plik
-    mov eax, [rsp + 24]     ; st_mode (offset 24)
-    and eax, 0xF000         ; maska dla typu pliku
-    cmp eax, S_IFREG        ; czy zwykły plik?
-    jne .not_regular        ; błąd jeśli nie
+    ; Sprawdza czy plik jest zwykly
+    mov eax, [rsp+ST_MODE]
+    and eax, 0xF000         ; sprawdza tylko typ
+    cmp eax, S_IFREG        ; porownuje z kodem zwyklego pliku
+    jne .revert_stack
 
-    mov r13, [rsp + 48]     ; st_size (offset 48)
-    add rsp, 144            ; przywróć stos
+    mov r13, [rsp+ST_SIZE]
+    add rsp, STAT_SIZE      ; zwolnij bufor na struct stat
 
-    ; Plik za krótki do odwracania?
+    ; Sprawdza czy plik nie jest za krotki
+    ; jesli plik ma mniej niz 2 bajty to nie trzeba go odwracac
     cmp r13, 2
-    jl .close_file          
+    jl .close_file
 
-    ; Mapowanie pliku
-    mov rax, 9              ; sys_mmap
+    ; Mapuje plik do pamieci
+    mov rax, SYS_MMAP
     xor rdi, rdi            ; addr = NULL
-    mov rsi, r13            ; rozmiar pliku
+    mov rsi, r13            ; length = rozmiar pliku w r13
     mov rdx, PROT_READ | PROT_WRITE
-    mov r10, MAP_SHARED     ; flags
+    mov r10, MAP_SHARED
     mov r8, r12             ; deskryptor
     xor r9, r9              ; offset = 0
     syscall
-    test rax, rax           ; sprawdź błąd
-    js .close_error         
-    mov r14, rax            ; zapisz adres mapy
+    test rax, rax
+    js .close_error
+    mov r14, rax            ; r14 = adres mapowanej pamieci
 
-    ; Odwracanie zawartości
-    mov rdi, rax            ; początek danych
-    lea rsi, [rax + r13 - 1] ; koniec danych
+    ; Odwraca plik
+    mov rdi, rax            ; rdi = poczatek
+    lea rsi, [rax+r13-1]    ; rsi = koniec
 .reverse_loop:
-    cmp rdi, rsi            
-    jge .after_reverse      
-    mov al, [rdi]           
-    mov cl, [rsi]
-    mov [rdi], cl
+    cmp rdi, rsi
+    jge .after_reverse      ; jesli wskazniki sie minely => koniec
+    mov al, [rdi]           ; al = bajt z poczatku
+    mov cl, [rsi]           ; cl = bajt z konca
+    mov [rdi], cl           ; swap
     mov [rsi], al
-    inc rdi                 
-    dec rsi                 
+    inc rdi
+    dec rsi
     jmp .reverse_loop
 
+    ; Zapisuje zmiany na dysku
 .after_reverse:
-    ; Synchronizuj z dyskiem
-    mov rax, 26             ; sys_msync
-    mov rdi, r14            ; adres mapy
+    mov rax, SYS_MSYNC
+    mov rdi, r14            ; adres mapowanej pamieci
     mov rsi, r13            ; rozmiar
-    mov rdx, MS_SYNC        ; flagi
+    mov rdx, MS_SYNC        ; flaga: MS_SYNC
     syscall
-    test rax, rax           
-    js .unmap_close_error   
+    test rax, rax
+    js .unmap_close_error
 
-    ; Odmapuj plik
-    mov rax, 11             ; sys_munmap
-    mov rdi, r14            
-    mov rsi, r13            
+    ; Zwalnia zmapowana pamiec
+    mov rax, SYS_MUNMAP
+    mov rdi, r14            ; adres mapowanej pamieci
+    mov rsi, r13            ; rozmiar
     syscall
-    test rax, rax           
-    js .close_error         
+    test rax, rax
+    js .close_error
 
+    ; Zamyka plik
 .close_file:
-    ; Zamknij plik
-    mov rax, 3              ; sys_close
-    mov rdi, r12            
+    mov rax, SYS_CLOSE
+    mov rdi, r12            ; deskryptor
     syscall
-    test rax, rax           
-    js .error               
+    test rax, rax
+    js .error
 
-    ; Zakończ sukcesem
-    xor rdi, rdi            
+    ; Konczy program z sukcesem
+    xor rdi, rdi            ; kod wyjscia = 0
     jmp .exit
 
+    ; Sprzata po bledzie sys_msync
 .unmap_close_error:
-    ; Sprzątanie po błędzie msync
-    mov rax, 11             ; sys_munmap
-    mov rdi, r14
-    mov rsi, r13
+    ; Zwalnia zmapowana pamiec
+    mov rax, SYS_MUNMAP
+    mov rdi, r14            ; adres mapowanej pamieci
+    mov rsi, r13            ; rozmiar
     syscall
-
-.fstat_error:
-    ; Przywróć stos po błędzie fstat
-    add rsp, 144
     jmp .close_error
 
-.not_regular:
-    ; Obsłuż nie-zwykły plik
-    add rsp, 144
+    ; Przywraca stos
+.revert_stack:
+    add rsp, STAT_SIZE
     jmp .close_error
 
+    ; Zamyka plik po bledzie
 .close_error:
-    ; Zamknij plik po błędzie
-    mov rax, 3              ; sys_close
+    mov rax, SYS_CLOSE
     mov rdi, r12
     syscall
 
+    ; Ustawia kod wyjscia na 1
 .error:
-    ; Zakończ z błędem
     mov rdi, 1
 
+    ; Konczy program
 .exit:
-    ; Wyjdź
-    mov rax, 60             ; sys_exit
+    mov rax, SYS_EXIT
     syscall
